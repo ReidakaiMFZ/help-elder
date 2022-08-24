48ad69dd6820b1aa444635596da829118df8bd56
Caua Rojas <cauarojas@hotmail.com>
2022-08-24 17:25:50 -0300

    added a login main page

--- help_elder/lib/main.dart
+++ help_elder/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_literals_to_create_immutables
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +13,30 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(
+          title: const Text('Help Elder'),
+          backgroundColor: Colors.black,
+        ),
+        body: Column(
+          children: [
+            TextField(
+              decoration: const InputDecoration(
+                labelText: 'Login',
+              ),
+            ),
+            TextField(
+              decoration: const InputDecoration(
+                labelText: 'Password',
+              ),
+            ),
+            ElevatedButton(
+              child: const Text('Login'),
+              onPressed: () {
+                Navigator.pushNamed(context, '/home');
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
