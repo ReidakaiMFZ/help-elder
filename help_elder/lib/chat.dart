2a6de9f76972dfda11875ed6fa9e57324f030063
ReidakaiMFZ <richardbsanches@outlook.com>
2022-09-22 16:58:56 -0300

    update debug page

---
+++ help_elder/lib/chat.dart
@@ -0,0 +1,22 @@
+// import 'package:firebase_auth/firebase_auth.dart';
+// ignore_for_file: prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+class Chat extends StatefulWidget {
+  const Chat({Key? key}) : super(key: key);
+
+  @override
+  ChatState createState() => ChatState();
+  
+  
+}
+
+class ChatState extends State<Chat>{
+  @override
+  Widget build (BuildContext context){
+    return const Scaffold(
+      
+    );
+  }
+}
