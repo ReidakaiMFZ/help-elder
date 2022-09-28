36f6b1f334cb576b1556b722d42052b8d0339268
Caua Rojas <cauarojas@hotmail.com>
2022-09-28 16:33:14 -0300

    adicionei coruja

--- help_elder/lib/home.dart
+++ help_elder/lib/home.dart
@@ -5,15 +5,20 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Help Elder'),
           backgroundColor: Colors.black,
         ),
-        body: const SizedBox(
-            width: 500, height: 500, child: Text('Tela principal')),
+        body: Column(
+          children: const [
+            Text('Tela principal'),
+            Image(
+                image: NetworkImage(
+                    'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'))
+          ],
+        ),
         backgroundColor: const Color.fromARGB(225, 235, 249, 255),
       ),
     );
